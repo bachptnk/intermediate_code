f4eae0c8d7906de3e98b5cf670f93514c2370306
Phạm Quang Bách <91135899+bachptnk@users.noreply.github.com>
2023-09-15 00:18:57 +0700

    Add files via upload

---
+++ [model-get-blenderbot].py
@@ -0,0 +1,8 @@
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+
+mname = "facebook/blenderbot-400M-distill"
+model = BlenderbotForConditionalGeneration.from_pretrained(mname)
+tokenizer = BlenderbotTokenizer.from_pretrained(mname)
+
+_ = tokenizer.save_pretrained("./model-dir")
+_ = model.save_pretrained("./model-dir")
